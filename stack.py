b55802b6c5ba4a1d61387d01424d4e65d1a3ee6a
Nagasuruthika N <66667949+Nagasuruthika@users.noreply.github.com>
2020-10-17 21:29:57 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -42,5 +42,38 @@ a
 Stack after elements are poped:
 deque([])
 
+# Python program to 
+# demonstrate stack implementation
+# using queue module
+
+
+from queue import LifoQueue
+
+# Initializing a stack
+stack = LifoQueue(maxsize = 3)
+
+# qsize() show the number of elements
+# in the stack
+print(stack.qsize())
+
+# put() function to push
+# element in the stack
+stack.put('a')
+stack.put('b')
+stack.put('c')
+
+print("Full: ", stack.full()) 
+print("Size: ", stack.qsize()) 
+
+# get() fucntion to pop
+# element from stack in 
+# LIFO order
+print('\nElements poped from the stack')
+print(stack.get())
+print(stack.get())
+print(stack.get())
+
+print("\nEmpty: ", stack.empty())
+
 
 
